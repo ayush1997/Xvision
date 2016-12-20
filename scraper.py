c70f3ef9fe97cf938ed06791aad2b96eeb7ee2cc
ayush <ayushkumarsingh97@gmail.com>
2016-12-20 17:33:22 +0530

    scraper added

---
+++ scraper.py
@@ -0,0 +1,66 @@
+from lxml import html
+import requests
+import re
+import json
+import urllib
+
+domain = 'https://openi.nlm.nih.gov/'
+url_list = []
+for i in range(55,75):
+    url = 'https://openi.nlm.nih.gov/gridquery.php?q=&it=x,xg&sub=x&m='+str(1+100*i)+'&n='+str(100+100*i)
+    url_list.append(url)
+regex = re.compile(r"var oi = (.*);")
+final_data = {}
+img_no = 5500
+
+
+def extract(url):
+    global img_no
+
+    img_no += 1
+    r = requests.get(url)
+    tree = html.fromstring(r.text)
+
+    div = tree.xpath('//table[@class="masterresultstable"]\
+        //div[@class="meshtext-wrapper-left"]')
+
+    if div != []:
+        div = div[0]
+    else:
+        return
+
+    typ = div.xpath('.//strong/text()')[0]
+    items = div.xpath('.//li/text()')
+    img = tree.xpath('//img[@id="theImage"]/@src')[0]
+
+
+    final_data[img_no] = {}
+    final_data[img_no]['type'] = typ
+    final_data[img_no]['items'] = items
+    final_data[img_no]['img'] = domain + img
+    urllib.urlretrieve(domain+img, "/home/ayush/Documents/xray/scraper/latest/"+str(img_no)+".png")
+    with open('data_new.json', 'w') as f:
+        json.dump(final_data, f)
+    print final_data[img_no]
+
+
+def main():
+    for url in url_list :
+        r = requests.get(url)
+        tree = html.fromstring(r.text)
+
+        script = tree.xpath('//script[@language="javascript"]/text()')[0]
+
+        json_string = regex.findall(script)[0]
+        json_data = json.loads(json_string)
+
+        next_page_url = tree.xpath('//footer/a/@href')
+
+        print 'extract'
+        links = [domain + x['nodeRef'] for x in json_data]
+        for link in links:
+            extract(link)
+
+if __name__ == '__main__':
+
+    main()
